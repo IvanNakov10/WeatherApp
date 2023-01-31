d9a15a52d062984e7fd6c6eb964138a418cb7e4b
Nakov <77243772+IvanNakov10@users.noreply.github.com>
2023-01-31 23:34:43 +0200

    done

--- weatherApp.py
+++ weatherApp.py
@@ -1,5 +1,14 @@
 import requests
+import key
 
 city = "Razgrad"
 
-url = f""
+url = f"api.openweathermap.org/data/2.5/forecast?id=524901&appid={key.key}"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print("Failed to retrieve weather data.")
